bbc7a1ee31732afead5db9f552669cf83b2c3bb9
Mikhail Zhirnov <antiphysic@gmail.com>
2020-01-25 16:44:45 +0300

    activity_add_note.xml

--- app/src/main/java/com/example/noteapp/database/NotesDatabase.kt
+++ app/src/main/java/com/example/noteapp/database/NotesDatabase.kt
@@ -1,7 +1,6 @@
 package com.example.noteapp.database
 
 import android.content.Context
-import android.provider.CalendarContract
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
