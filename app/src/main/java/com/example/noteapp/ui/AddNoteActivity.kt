77a2ea2e468291c5667783054dac76448d64bbe2
Mikhail Zhirnov <antiphysic@gmail.com>
2020-01-25 23:04:43 +0300

    AddNoteActivity

--- app/src/main/java/com/example/noteapp/ui/AddNoteActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/AddNoteActivity.kt
@@ -1,13 +1,59 @@
 package com.example.noteapp.ui
 
+import com.example.noteapp.R
+import com.example.noteapp.database.Note
+import com.example.noteapp.database.NotesDatabase
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.noteapp.R
+import kotlinx.android.synthetic.main.activity_add_note.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class AddNoteActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity(), CoroutineScope {
+
+
+    private var noteDB : NotesDatabase?= null
+    private lateinit var mJob: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = mJob + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+
+        mJob = Job()
+        noteDB =
+            NotesDatabase.getDatabase(this)
+
+        addBtn.setOnClickListener {
+            val strTitle: String = titleEt.text.toString()
+            val strDesc: String = descriptionEt.text.toString()
+
+
+            if (strTitle.isEmpty()) {
+                titleEt.error = "title required"
+                titleEt.requestFocus()
+                return@setOnClickListener
+            }
+            if (strDesc.isEmpty()) {
+                descriptionEt.error = "description required"
+                descriptionEt.requestFocus()
+                return@setOnClickListener
+            }
+            launch {
+                noteDB?.notesDao()?.insert(Note(title = strTitle, desc = strDesc))
+                finish()
+            }
+        }
     }
-}0
+    override fun onDestroy() {
+        super.onDestroy()
+        mJob.cancel()
+    }
+}
