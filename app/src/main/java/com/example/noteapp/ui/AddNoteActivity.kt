e24b410642b1b494a3ca720734ea748c2eca9abe
Mikhail Zhirnov <antiphysic@gmail.com>
2020-01-25 15:53:23 +0300

    AddNoteActivity

--- app/src/main/java/com/example/noteapp/ui/AddNoteActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/AddNoteActivity.kt
@@ -1,12 +1,13 @@
-package com.example.noteapp
+package com.example.noteapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.noteapp.R
 
-class MainActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_note)
     }
 }
